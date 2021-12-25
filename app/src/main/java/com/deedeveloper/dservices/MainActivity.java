110d8d5dd2d82d1e2a943b127967fd8659333281
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2021-12-25 23:50:11 +0530

    splash: hide status bar
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- app/src/main/java/com/deedeveloper/dservices/MainActivity.java
+++ app/src/main/java/com/deedeveloper/dservices/MainActivity.java
@@ -1,8 +1,9 @@
 package com.deedeveloper.dservices;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
