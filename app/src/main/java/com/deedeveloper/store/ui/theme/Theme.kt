20cd2e308464ac1db16342885b466448650c9efe
Deepak5310 <37282143+Deepak5310@users.noreply.github.com>
2021-12-28 20:19:49 +0530

    kt: remove unnecessary code
    Signed-off-by: Deepak5310 <37282143+Deepak5310@users.noreply.github.com>

--- app/src/main/java/com/deedeveloper/store/ui/theme/Theme.kt
+++ app/src/main/java/com/deedeveloper/store/ui/theme/Theme.kt
@@ -16,19 +16,10 @@ private val LightColorPalette = lightColors(
     primary = Purple500,
     primaryVariant = Purple700,
     secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
 )
 
 @Composable
-fun StoreTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun StoreTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
